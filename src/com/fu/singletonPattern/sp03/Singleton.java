805f1ada95cc23f96b867b5ec5818c7f5bb32215
fq1995 <1152237929@qq.com>
2017-11-13 16:44:44 +0800

    饿汉式单例

---
+++ src/com/fu/singletonPattern/sp03/Singleton.java
@@ -0,0 +1,12 @@
+package com.fu.singletonPattern.sp03;
+
+public class Singleton {
+	private static Singleton instance = new Singleton();
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
